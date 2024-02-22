aeb18a823deb3d6ebc1780510f48a1603f664fbe
4MarkusMarkus <4markusmarkus@gmail.com>
2024-02-22 19:27:59 +0100

    removed old rpl datasets

--- rocket_rewards_analysis.py
+++ rocket_rewards_analysis.py
@@ -1,33 +1,33 @@
 import pandas as pd
 
 # Load the CSV file
-file_path = "lido_stakers_balances.csv"  # Replace with your file path
+file_path = "rocket_staking_snapshot_2.csv"  # Replace with your file path
 data = pd.read_csv(file_path)
 
 # Sort the data based on the 'value' column
-data = data.sort_values(by="value", ascending=False)
+data = data.sort_values(by="provided_eth", ascending=False)
 
 # Calculate total sum, mean, and median for the entire dataset
-total_sum = data["value"].sum()
-total_mean = data["value"].mean()
-total_median = data["value"].median()
+total_sum = data["provided_eth"].sum()
+total_mean = data["provided_eth"].mean()
+total_median = data["provided_eth"].median()
 
 
 def print_percentile_stats(start_percentile, end_percentile, step):
     cumulative_sum_ratio = 0  # Initialize cumulative sum ratio
 
     for percentile in range(start_percentile, end_percentile, step):
-        lower_bound = data["value"].quantile(percentile / 100.0)
-        upper_bound = data["value"].quantile((percentile + step) / 100.0)
+        lower_bound = data["provided_eth"].quantile(percentile / 100.0)
+        upper_bound = data["provided_eth"].quantile((percentile + step) / 100.0)
 
         values_in_percentile = data[
-            (data["value"] <= upper_bound) & (data["value"] >= lower_bound)
+            (data["provided_eth"] <= upper_bound) & (data["provided_eth"] >= lower_bound)
         ]
 
-        percentile_sum = values_in_percentile["value"].sum()
-        percentile_mean = values_in_percentile["value"].mean()
-        percentile_median = values_in_percentile["value"].median()
-        percentile_std_err = values_in_percentile["value"].sem()
+        percentile_sum = values_in_percentile["provided_eth"].sum()
+        percentile_mean = values_in_percentile["provided_eth"].mean()
+        percentile_median = values_in_percentile["provided_eth"].median()
+        percentile_std_err = values_in_percentile["provided_eth"].sem()
 
         sum_ratio = percentile_sum / total_sum
         cumulative_sum_ratio += sum_ratio  # Update cumulative sum ratio
@@ -35,8 +35,8 @@ def print_percentile_stats(start_percentile, end_percentile, step):
         median_ratio = percentile_median / total_median
 
         print(f"Percentile {percentile}-{percentile + step}:")
-        print(f"  First value: {values_in_percentile.iloc[0]['value']}")
-        print(f"  Last value: {values_in_percentile.iloc[-1]['value']}")
+        print(f"  First value: {values_in_percentile.iloc[0]['provided_eth']}")
+        print(f"  Last value: {values_in_percentile.iloc[-1]['provided_eth']}")
         print(f"  Sum Ratio: {sum_ratio:.2f}")
         print(
             f"  Cumulative Sum Ratio: {cumulative_sum_ratio:.2f}"
